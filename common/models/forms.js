ddf25c60f75d9a9dd50f152dbc16142297404064
bshrram <bshoor96@gmail.com>
2018-07-25 08:33:23 +0300

    Update forms.js

--- common/models/forms.js
+++ common/models/forms.js
@@ -2,7 +2,8 @@
 let app = require('../../server/server.js');
 
 module.exports = function (Forms) {
-    
+
+
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     Forms.validatesFormatOf('Email', { with: re, message: 'Must provide a valid email' });
     Forms.validatesUniquenessOf('MobileNo');
