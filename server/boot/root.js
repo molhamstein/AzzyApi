f6cfc8e489ee2427815dc891640c941fbcdea718
bshrram <bshoor96@gmail.com>
2018-11-21 20:12:59 +0200

    Update root.js

--- server/boot/root.js
+++ server/boot/root.js
@@ -5,7 +5,8 @@ module.exports = function(server) {
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
   router.get('/contractPdf/:name' , function (req ,res){
-    res.sendFile('./contractsPDF/' + req.params.name);
+    console.log('./contractsPDF/' + req.params.name)
+    res.sendfile('./contractsPDF/' + req.params.name);
   });
   server.use(router);
 };
