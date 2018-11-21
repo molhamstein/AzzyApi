2ab1d9a6d8d50609c2efa492e128f91e332804f6
bshrram <bshoor96@gmail.com>
2018-11-21 22:28:55 +0200

    Update forms.js

--- common/models/forms.js
+++ common/models/forms.js
@@ -702,8 +702,8 @@ module.exports = function (Forms) {
                 html_body = html_body.split("{{image}}").join(image);
                 var options = {
                     renderDelay: 1000,
-                    height: "1244.7px",
-                    width: "880px",
+                    height: "1273px",
+                    width: "900px",
                     border: {
                         top: "18px", 
                         right: "0px",
