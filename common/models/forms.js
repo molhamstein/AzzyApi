e4806b50fbb3c779c82f01e41a9b7efb4e11ff07
bshrram <bshoor96@gmail.com>
2018-11-21 22:02:49 +0200

    Update forms.js

--- common/models/forms.js
+++ common/models/forms.js
@@ -702,8 +702,8 @@ module.exports = function (Forms) {
                 html_body = html_body.split("{{image}}").join(image);
                 var options = {
                     renderDelay: 1000,
-                    width: "900px" ,
-                    height: "1273px",
+                    width: "880px" ,
+                    height: "1244.7px",
                     orientation: "portrait"
                 }
                 pdf.create(html_body,options).toFile('./contractsPDF/' + file,function(err,res){
