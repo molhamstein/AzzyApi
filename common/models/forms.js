7949dfd24dd12e066938b5a35900ac46923e558d
bshrram <bshoor96@gmail.com>
2018-12-16 16:47:43 +0200

    Update forms.js

--- common/models/forms.js
+++ common/models/forms.js
@@ -746,7 +746,7 @@ module.exports = function (Forms) {
                 }
                 pdf.create(html_body, options).toFile('./contractsPDF/' + file, function (err, res) {
                     if (err) return cb(err);
-                    cb(null, { url: config.baseURL + "/contractPdf/" + file });
+                    cb(null, { url: "http://azzyimmigration.com:3000" + "/contractPdf/" + file });
                 })
                 /*
                 const htmlToPDF = new HTMLToPDF({
