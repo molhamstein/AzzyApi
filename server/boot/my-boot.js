ac2614503e8495b8e27d5dfa33494fb4e51a1e77
anas-1995 <anasalazmeh.95@gmail.com>
2019-02-24 15:58:28 +0200

    pdf feedback5

--- server/boot/my-boot.js
+++ server/boot/my-boot.js
@@ -1,96 +1,96 @@
-// module.exports = function (app) {
-//   var User = app.models.staffuser;
-//   var Role = app.models.Role;
-//   var RoleMapping = app.models.RoleMapping;
-//   var _ = require('lodash');
+module.exports = function (app) {
+  var User = app.models.staffuser;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+  var _ = require('lodash');
 
-//   var counter= app.models.counters;
-//   counter.create({
-//     value: 0,
-//     collection: "client"
-//   }, function (err ,res){
-//     if (err) throw err;
-//   });
+  var counter= app.models.counters;
+  counter.create({
+    value: 0,
+    collection: "client"
+  }, function (err ,res){
+    if (err) throw err;
+  });
 
-//   Role.create({
-//     name: 'manager'
-//   }, function (err, role) {
-//     if (err) throw err;
+  Role.create({
+    name: 'manager'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
-//   Role.create({
-//     name: 'secretary'
-//   }, function (err, role) {
-//     if (err) throw err;
+    console.log('Created role:', role);
+  });
+  Role.create({
+    name: 'secretary'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
+    console.log('Created role:', role);
+  });
 
-//   Role.create({
-//     name: 'consultant'
-//   }, function (err, role) {
-//     if (err) throw err;
+  Role.create({
+    name: 'consultant'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
+    console.log('Created role:', role);
+  });
 
-//   Role.create({
-//     name: 'adminstrator'
-//   }, function (err, role) {
-//     if (err) throw err;
+  Role.create({
+    name: 'adminstrator'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
+    console.log('Created role:', role);
+  });
 
-//   Role.create({
-//     name: 'formEditer'
-//   }, function (err, role) {
-//     if (err) throw err;
+  Role.create({
+    name: 'formEditer'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
+    console.log('Created role:', role);
+  });
 
-//   Role.create({
-//     name: 'appSelecter'
-//   }, function (err, role) {
-//     if (err) throw err;
+  Role.create({
+    name: 'appSelecter'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
+    console.log('Created role:', role);
+  });
 
-//   Role.create({
-//     name: 'appCanceler'
-//   }, function (err, role) {
-//     if (err) throw err;
+  Role.create({
+    name: 'appCanceler'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
+    console.log('Created role:', role);
+  });
   
-//   Role.create({
-//     name: 'reception'
-//   }, function (err, role) {
-//     if (err) throw err;
+  Role.create({
+    name: 'reception'
+  }, function (err, role) {
+    if (err) throw err;
 
-//     console.log('Created role:', role);
-//   });
+    console.log('Created role:', role);
+  });
   
 
-//   var managers = [{type: 'manager', username: 'admin', email: 'control@azzyimmi.com', password: 'password', emailVerified: true }];
-//   User.findOne({ where: { email: managers[0].email } }, function (err, manager) {
-//     if (err) throw err;
+  var managers = [{type: 'manager', username: 'admin', email: 'control@azzyimmi.com', password: 'password', emailVerified: true }];
+  User.findOne({ where: { email: managers[0].email } }, function (err, manager) {
+    if (err) throw err;
 
-//     if (_.isEmpty(manager)) {
-//       User.create(managers, function (err, users) {
-//         if (err) throw err;
-//         console.log('Created users:', users);
-//         User.addRole(users[0].id, 1, console.log);
-//       })
-//     }
-//     else {
-//       User.addRole(manager.id, 1, console.log);
-//     }
+    if (_.isEmpty(manager)) {
+      User.create(managers, function (err, users) {
+        if (err) throw err;
+        console.log('Created users:', users);
+        User.addRole(users[0].id, 1, console.log);
+      })
+    }
+    else {
+      User.addRole(manager.id, 1, console.log);
+    }
 
-//   });
+  });
 
-// };
+};
